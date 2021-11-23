aca46e555bcef85eb5813555b14b9852354a82a3
Emilorzz <emil69h@gmail.com>
2021-11-23 09:49:31 +0100

    added call to loadData in program/main

--- src/program/program.c
+++ src/program/program.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include "loadPseudoData.h"
 #include "sum.h"
 
 int main(void) {
-    printf("5 + 4 = %i\n", sum(5, 4));
+    loadData("src/shared/input.txt", "src/shared/output.txt");
 
     return EXIT_SUCCESS;
 }
--- src/shared/loadPseudoData.h
+++ src/shared/loadPseudoData.h
@@ -1,2 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
+
 int loadData(const char *input_file, const char *output_file);
 void copyData(FILE *ifp, FILE *ofp);
