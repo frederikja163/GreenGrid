1462b0f2d059313d18685c6ba4fd7d7730c9ad84
AleksanderRDL <55142567+AleksanderRDL@users.noreply.github.com>
2021-11-24 12:20:05 +0100

    Data now in formats of 10 minutes

--- src/shared/calculateOptimalTime.c
+++ src/shared/calculateOptimalTime.c
@@ -4,9 +4,9 @@
 #include "calculateOptimalTime.h"
 
     /* Function to load specific emission data in each timeslot */
-void load_emission_data(emission_time *et, double emissions, int hourFromRequest) {
+void load_emission_data(emission_time *et, double emissions, int minsFromRequest) {
     et->emissions = emissions;
-    et->hour = hourFromRequest;
+    et->min = minsFromRequest;
 }
 
     /* compare function, lowest emission value gets pushed first 
@@ -27,18 +27,19 @@ void load_all_data(emission_time *data) {
     for(i = 0; i < DATA_SIZE; i++) {
             postiveOrNegative = rand()%2;
             if (postiveOrNegative) {
-                currentEmissions = currentEmissions + rand()%500;
+                currentEmissions = currentEmissions + rand()%100;
             } else {
-                currentEmissions = currentEmissions + (rand()%500)*-1;
+                currentEmissions = currentEmissions + (rand()%100)*-1;
             }
-            load_emission_data(&data[i], currentEmissions, i);
-            printf ("Hours from API request = %d, Emissions = %.2lf \n",data[i].hour, data[i].emissions);   
+            load_emission_data(&data[i], currentEmissions, i*10);
+            printf ("Min from API request = %d, Emissions = %.2lf \n",data[i].min, data[i].emissions);   
     }
 }
 
     /* Algorithm to find the most optimal time to run based 
        on array of emission data and the device's hours active */
 int find_optimal_time(int activeHours) {
+    activeHours *= 6;
     int i, j;
     double currentEmissions = 0;
     double lowestEmissions = 999999999; 
@@ -55,7 +56,7 @@ int find_optimal_time(int activeHours) {
         }
         if (currentEmissions < lowestEmissions) {
             lowestEmissions = currentEmissions;
-            optimalTime = data[i].hour;
+            optimalTime = data[i].min;
         }
     } 
     return optimalTime;
--- src/shared/calculateOptimalTime.h
+++ src/shared/calculateOptimalTime.h
@@ -1,13 +1,14 @@
-#define DATA_SIZE (7*24)
+    /* 7 days in a week, 24 hours a day, 6*10 minutes in an hour */
+#define DATA_SIZE (7*24*6)
 
 struct emission_time
 {
     double emissions;
-    int hour;
+    int min;
 };
 typedef struct emission_time emission_time;
 
-/*int compare_function (const void *ptr1, const void *ptr2); */
-void load_emission_data(emission_time *et, double emissions, int hour);
+/* int compare_function (const void *ptr1, const void *ptr2); */
+void load_emission_data(emission_time *et, double emissions, int min);
 int find_optimal_time(int activeHours);
 void load_all_data(emission_time *data);
