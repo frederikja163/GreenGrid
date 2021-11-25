2d71b05dbfe24529dd479ea605ce35e66e179b11
Anders Aavild <anders@aavild.dk>
2021-11-25 12:20:31 +0100

    Since there were issues implementing the library cURL, the code for downloading data from DMI has been commented out for now. Also added usage of cJSON for converting the JSON string into a tree and take out relevant info, timeserie and timestamp for how old the data is and returns this to the functioncaller. Currently untested

--- src/shared/DMIParser.c
+++ src/shared/DMIParser.c
@@ -2,43 +2,16 @@
  * Created by Aavild on 19/11/2021.
  */
 #include <stdlib.h>
-#include "DMIParser.h"
+#include <stdio.h>
+#include <string.h>
 #include <cJSON.h>
-#include <curl/curl.h>
+#include "DMIParser.h"
+/*#include <curl/curl.h>*/
 
-typedef struct memory {
-    char *response;
-    size_t size;
-} memory;
-typedef struct windValue {
-    int timestamp;
-    double windspeed;
-} windValue;
-
-static size_t cb(void *data, size_t size, size_t nmemb, void *userp);
-windValue* ParseStringToWind(const char* string);
-memory GetDMIData(void);
-
-static size_t cb(void *data, size_t size, size_t nmemb, void *userp)
+/*input ninjo2dnudk.json and you will get out an array of windValues and a timestamp for last update*/
+windValue* ParseStringToWind(const char *input, int *lastUpdate)
 {
-    size_t realsize = size * nmemb;
-    struct memory *mem = (struct memory *)userp;
-
-    char *ptr = realloc(mem->response, mem->size + realsize + 1);
-    if(ptr == NULL)
-        return 0;  /* out of memory! */
-
-    mem->response = ptr;
-    memcpy(&(mem->response[mem->size]), data, realsize);
-    mem->size += realsize;
-    mem->response[mem->size] = 0;
-
-    return realsize;
-}
-
-windValue* ParseStringToWind(const char* string)
-{
-    cJSON *json = cJSON_Parse(string);
+    cJSON *json = cJSON_Parse(input);
     if (json == NULL)
     {
         const char *error = cJSON_GetErrorPtr();
@@ -49,20 +22,19 @@ windValue* ParseStringToWind(const char* string)
         return 0;
     }
 
-    cJSON *id = cJSON_GetObjectItem(json, "id");
-    if (cJSON_IsString(id) && (id->valuestring != NULL))
+    cJSON *lastUpdateJson = cJSON_GetObjectItem(json, "lastupdate");
+    if (cJSON_IsString(lastUpdateJson) && (lastUpdateJson->valueint != 0))
     {
-        printf("Checking id \"%s\"\n", id->valuestring);
+        printf("Input file last updated: \"%i\"\n", lastUpdateJson->valueint);
     }
-    cJSON_Delete(json);
     /*Test hertil i første omgang*/
-    windValue *values = malloc(sizeof(windValue)*100);
+    windValue *values = malloc(sizeof(windValue)*95); /*timeserie of size 95*/
     if(values == NULL)
         return 0;  /* out of memory! */
     int iterator = 0;
     cJSON *timeserie = cJSON_GetObjectItem(json, "timeserie");
     cJSON *hour;
-    /*cJSON_ArrayForEach(hour, timeserie)
+    cJSON_ArrayForEach(hour, timeserie)
     {
         cJSON *time = cJSON_GetObjectItem(hour, "time");
         cJSON *windSpeed = cJSON_GetObjectItem(hour, "windSpeed");
@@ -74,32 +46,49 @@ windValue* ParseStringToWind(const char* string)
         values[iterator].timestamp = time->valueint;
         values[iterator].windspeed = hour->valuedouble;
         iterator++;
-    }*/
+    }
 
     cJSON_Delete(json);
     return values;
 }
 
-memory GetDMIData(void)
+/*cURL*/
+/*static size_t cb(void *data, size_t size, size_t nmemb, void *userp)
+{
+    size_t realsize = size * nmemb;
+    struct memory *mem = (struct memory *)userp;
+
+    char *ptr = realloc(mem->response, mem->size + realsize + 1);
+    if(ptr == NULL)*/
+/*        return 0;  *//* out of memory! */
+/*
+    mem->response = ptr;
+    memcpy(&(mem->response[mem->size]), data, realsize);
+    mem->size += realsize;
+    mem->response[mem->size] = 0;
+
+    return realsize;
+}*/
+/*memory GetDMIData(void)
 {
     memory data;
     CURL *curl = curl_easy_init();
-    if(curl) {
+    if(curl) {*/
         /*https://www.dmi.dk/NinJo2DmiDk/ninjo2dmidk
          * opts from https://stackoverflow.com/questions/59454006/how-to-use-curl-in-c-and-parse-json-response
          * */
-        curl_easy_setopt(curl, CURLOPT_URL, "https://www.dmi.dk/NinJo2DmiDk/ninjo2dmidk?cmd=llj&ids=2624886");
+        /*curl_easy_setopt(curl, CURLOPT_URL, "https://www.dmi.dk/NinJo2DmiDk/ninjo2dmidk?cmd=llj&ids=2624886");
         curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "GET");
         curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
         curl_easy_setopt(curl, CURLOPT_DEFAULT_PROTOCOL, "https");
-
+         */
         /*https://curl.se/libcurl/c/CURLOPT_URL.html
          * https://curl.se/libcurl/c/CURLOPT_WRITEFUNCTION.html*/
 
         /* send all data to this function  */
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, cb);
+        /*curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, cb);*/
         /* we pass our 'chunk' struct to the callback function */
-        memory chunk = {0};
+        /*memory chunk = {0};
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&chunk);
 
         curl_easy_perform(curl);
@@ -108,4 +97,4 @@ memory GetDMIData(void)
     }
     curl_easy_cleanup(curl);
     return data;
-}
+}*/
--- src/shared/DMIParser.h
+++ src/shared/DMIParser.h
@@ -2,6 +2,23 @@
  * Created by Aavild on 19/11/2021.
  */
 #ifndef CO2_DMIPARSER_H
+
 #define CO2_DMIPARSER_H
 
-#endif /*CO2_DMIPARSER_H*/
+typedef struct windValue {
+    int timestamp;
+    double windspeed;
+} windValue;
+
+windValue* ParseStringToWind(const char *input, int *lastUpdate);
+
+/*typedef struct memory {
+    char *response;
+    size_t size;
+} memory;*/
+
+/*static size_t cb(void *data, size_t size, size_t nmemb, void *userp);*/
+/*memory GetDMIData(void);*/
+
+#endif
+
