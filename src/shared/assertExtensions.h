ea88d7eb72b07672830a2581c94fd48b8cd6fb5b
Emilorzz <emil69h@gmail.com>
2021-11-26 10:50:43 +0100

    created assertExtensions.h for readability

---
+++ src/shared/assertExtensions.h
@@ -0,0 +1,4 @@
+#include <assert.h>
+
+#define assert_equal(a, b) assert((a) == (b))
+#define assert_not_equal(a, b) assert((a) != (b))
--- src/shared/loadPseudoData.c
+++ src/shared/loadPseudoData.c
@@ -1,23 +1,24 @@
 #include "loadPseudoData.h"
 
-#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
+
+#include "assertExtensions.h"
 
 char *read_file(const char *fileName) {
     FILE *inputFilePtr;
     char *fileContents;
     size_t fileSize = 0;
 
-    assert((inputFilePtr = fopen(fileName, "r")) != NULL);
+    assert_not_equal(inputFilePtr = fopen(fileName, "r"), NULL);
 
     fseek(inputFilePtr, 0, SEEK_END);
     fileSize = ftell(inputFilePtr);
     fseek(inputFilePtr, 0, SEEK_SET);
 
-    assert((fileContents = malloc((fileSize + 1) * sizeof(char))) != NULL);
+    assert_not_equal(fileContents = malloc((fileSize + 1) * sizeof(char)), NULL);
 
-    assert(fread(fileContents, sizeof(char), fileSize, inputFilePtr) == fileSize);
+    assert_equal(fread(fileContents, sizeof(char), fileSize, inputFilePtr), fileSize);
 
     fileContents[fileSize] = '\0';
 
@@ -28,7 +29,7 @@ char *read_file(const char *fileName) {
 
 void write_file(const char *fileName, const char *fileContents) {
     FILE *outputFilePtr;
-    assert((outputFilePtr = fopen(fileName, "w+")) != NULL);
+    assert_not_equal(outputFilePtr = fopen(fileName, "w+"), NULL);
 
     fputs(fileContents, outputFilePtr);
 
