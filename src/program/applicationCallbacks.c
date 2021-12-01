998a5cb9995593685a0ffce9e6facaef5aeb59e0
FrederikJA <fjaplv64@gmail.com>
2021-12-01 12:02:39 +0100

    defines for constants and organised text for the graph

--- src/program/applicationCallbacks.c
+++ src/program/applicationCallbacks.c
@@ -6,25 +6,47 @@
 
 #include "cairoExtensions.h"
 
+#define GRAPH_ARROW_LEG_LENGTH 10
+#define GRAPH_ARROW_ANGLE 0.8
+#define GRAPH_MARGIN_X 50
+#define GRAPH_MARGIN_Y 50
+#define GRAPH_SIZE_X 500
+#define GRAPH_SIZE_Y 500
+#define GRAPH_AXIS_LABEL_SIZE 10
+
 static int f(int x) {
     int value = (int)(cos((double)x / 3.141) * 100);
     return value;
 }
 
 static void on_draw(GtkDrawingArea *drawing_area, cairo_t *cr, int width, int height, gpointer data) {
+    cairo_select_font_face(cr, "Sans", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_NORMAL);
     cairo_set_source_rgb(cr, 0, 0, 0);
+    
     cairo_set_line_width(cr, 0.75);
-
     int i;
     for (i = 100; i < 200; i++) {
         cairo_line_to(cr, i + 1, f(i+1) + 200);
     }
     cairo_stroke(cr);
 
+    /* Draw arrows for axis. */
     cairo_set_line_width(cr, 2);
-    cairo_draw_arrow(cr, 50, 500, 50, 50, 10, 0.8);
-    cairo_draw_arrow(cr, 50, 500, 500, 500, 10, 0.8);
+    cairo_draw_arrow(cr,
+                        GRAPH_MARGIN_X, GRAPH_MARGIN_Y + GRAPH_SIZE_Y,
+                        GRAPH_MARGIN_X, GRAPH_MARGIN_Y,
+                        GRAPH_ARROW_LEG_LENGTH, GRAPH_ARROW_ANGLE);
+    cairo_draw_arrow(cr,
+                        GRAPH_MARGIN_X, GRAPH_SIZE_Y + GRAPH_MARGIN_Y,
+                        GRAPH_MARGIN_X + GRAPH_SIZE_X, GRAPH_MARGIN_Y + GRAPH_SIZE_Y,
+                        GRAPH_ARROW_LEG_LENGTH, GRAPH_ARROW_ANGLE);
     cairo_stroke(cr);
+
+    cairo_set_font_size(cr, GRAPH_AXIS_LABEL_SIZE);
+    cairo_move_to(cr, GRAPH_MARGIN_X + GRAPH_SIZE_X, GRAPH_MARGIN_Y + GRAPH_SIZE_Y);
+    cairo_show_text(cr, " Time");
+    cairo_move_to(cr, GRAPH_MARGIN_X, GRAPH_MARGIN_Y);
+    cairo_show_text(cr, " CO2");
 }
 
 void application_activate(GApplication *application, gpointer userdata) {
