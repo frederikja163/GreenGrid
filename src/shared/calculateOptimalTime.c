e1168a3eb6fe8bb5550ede2becdaaebc86d69a8a
AleksanderRDL <55142567+AleksanderRDL@users.noreply.github.com>
2021-12-01 12:48:11 +0100

    Now working with input data from DMI yay

--- src/shared/calculateOptimalTime.c
+++ src/shared/calculateOptimalTime.c
@@ -1,62 +1,36 @@
 /* Calculating emissions Poggers */
 #include <stdio.h>
 #include <time.h>
+#include <string.h>
 #include "calculateOptimalTime.h"
-
-    /* Function to load specific emission data in each timeslot */
-void load_emission_data(emission_time *et, double emissions, int hourFromRequest) {
-    et->emissions = emissions;
-    et->hour = hourFromRequest;
-}
-
-    /* compare function, lowest emission value gets pushed first 
-int compare_function (const void *ptr1, const void *ptr2) {
-    const emission_time *elem1 = ptr1;
-    const emission_time *elem2 = ptr2;
-
-    return (elem1->emissions - elem2->emissions);
-}
- */
-
-    /* loads random data in the input array of structs */
-void load_all_data(emission_time *data) {
-    int i;
-    int postiveOrNegative;
-    double currentEmissions = 10000;
-    srand(time(NULL));
-    for(i = 0; i < DATA_SIZE; i++) {
-            postiveOrNegative = rand()%2;
-            if (postiveOrNegative) {
-                currentEmissions = currentEmissions + rand()%500;
-            } else {
-                currentEmissions = currentEmissions + (rand()%500)*-1;
-            }
-            load_emission_data(&data[i], currentEmissions, i);
-            printf ("Hours from API request = %d, Emissions = %.2lf \n",data[i].hour, data[i].emissions);   
-    }
-}
+#include "DMIParser.h"
+#include "loadPseudoData.h"
 
     /* Algorithm to find the most optimal time to run based 
-       on array of emission data and the device's hours active */
-int find_optimal_time(int activeHours) {
+       on array of windspeed data and the device's hours active */
+char * find_optimal_time(int activeHours, char *optimalTime) {
     int i, j;
-    double currentEmissions = 0;
-    double lowestEmissions = 999999999; 
-    int optimalTime;
-    emission_time data[DATA_SIZE];
-    load_all_data(data);
-
-    printf("\n--------------------------------\n");
+    double currentWindspeed = 0;
+    double highestWindspeed = 0; 
+    char *inputString;
+    inputString = read_file("src/shared/ninjo2dmidk.json");
+    char *updateTimeStamp;
+    windValue* values = ParseStringToWind(inputString, updateTimeStamp);
     
     for(i = 0; i < DATA_SIZE-(activeHours+1); i++) { 
-        currentEmissions = 0;
+        currentWindspeed = 0;
         for(j = 0; j < activeHours; j++) {
-            currentEmissions += data[i+j].emissions;
+            currentWindspeed += values[i+j].windspeed;
         }
-        if (currentEmissions < lowestEmissions) {
-            lowestEmissions = currentEmissions;
-            optimalTime = data[i].hour;
+        if (currentWindspeed > highestWindspeed) {
+            highestWindspeed = currentWindspeed;
+            strcpy(optimalTime, values[i].timestamp);
         }
     } 
+    for (i = 0; i < 97; ++i) {
+        free(values[i].timestamp);
+    }
+    free(values);
+    free(inputString);
     return optimalTime;
 }
--- src/shared/calculateOptimalTime.h
+++ src/shared/calculateOptimalTime.h
@@ -1,13 +1,3 @@
-#define DATA_SIZE (7*24)
+#define DATA_SIZE 53
 
-struct emission_time
-{
-    double emissions;
-    int hour;
-};
-typedef struct emission_time emission_time;
-
-/*int compare_function (const void *ptr1, const void *ptr2); */
-void load_emission_data(emission_time *et, double emissions, int hour);
-int find_optimal_time(int activeHours);
-void load_all_data(emission_time *data);
+char * find_optimal_time(int activeHours, char *);
