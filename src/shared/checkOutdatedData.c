eea7773a7148b73f4606f59ab2ec1d0fcc5c8796
Vaalmigi Sukumar <vaalmigi8@gmail.com>
2021-12-02 12:50:53 +0100

    Hey sir this is my gift for 2 rupees for u, u like it i hope

--- src/shared/checkOutdatedData.c
+++ src/shared/checkOutdatedData.c
@@ -2,21 +2,21 @@
 #include <stdlib.h>
 #include <time.h>
 
-int check_outdated_data(int maxTimeDiff, const char *receivingData) {
+int check_outdated_data(int maxTimeDiff, const char * receivingData) {
     time_t curTime = time(NULL);
    /*  printf("Current timestamp = %ld\n", curTime); */
 
-    struct tm timeptr;
     /* We are now converting the recieving data(Charachter string) into a timestamp via 
     this function strptime and this timeptr struct. */
-    strptime(receivingData, "%Y%m%d%H%M%S", &timeptr);
-    time_t timeToStamp = mktime(&timeptr);
+    struct tm timePtr;
+    strptime(receivingData, "%Y%m%d%H%M%S", &timePtr);
+    time_t timeToStamp = mktime(&timePtr);
     /* printf("Receving timestamp = %ld\n", timetostamp); */
 
-    double timediff = difftime(curTime, timeToStamp);
+    double timeDiff = difftime(curTime, timeToStamp);
     /* printf("Difference between timestamps = %.0lf\n", timediff); */
     
-    if (timediff > maxTimeDiff) {
+    if (timeDiff > maxTimeDiff) {
         /* Fix new data */
         return 1;
     }
