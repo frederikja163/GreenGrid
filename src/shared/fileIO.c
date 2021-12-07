d0cb5b8a562a96ad15ccd76e58e93b4fcdca2b41
Emilorzz <emil69h@gmail.com>
2021-12-07 09:16:26 +0100

    added comments to fileIO.c and deleted an unused variable

--- src/shared/fileIO.c
+++ src/shared/fileIO.c
@@ -1,5 +1,5 @@
 #include "fileIO.h"
-/* To force files to use LF files endings if using any Windows OS */
+
 #ifdef _WIN32
 #include <fcntl.h>
 #include <io.h>
@@ -11,38 +11,45 @@
 
 #include "assertExtensions.h"
 
-char *read_file(const char *fileName) {
+char *read_file(const char *filePath) {
     FILE *inputFilePtr;
     char *fileContents;
     size_t fileSize = 0;
     size_t readSize;
 
-    assert_not_equal(inputFilePtr = fopen(fileName, "r"), NULL);
+    /* Open stream to filePath in read mode */
+    assert_not_equal(inputFilePtr = fopen(filePath, "r"), NULL);
 
+    /* Get size of file in bytes */
     fseek(inputFilePtr, 0, SEEK_END);
     fileSize = ftell(inputFilePtr);
     fseek(inputFilePtr, 0, SEEK_SET);
 
     assert_not_equal(fileContents = malloc((fileSize + 1) * sizeof(char)), NULL);
 
+    /* Read everything in file to fileContents */
     assert_equal(readSize = fread(fileContents, sizeof(char), fileSize, inputFilePtr), fileSize);
 
+    /* Convert array to null terminated string */
     fileContents[fileSize] = '\0';
 
+/* Force files to use LF files endings if using any Windows OS */
 #ifdef _WIN32
     _setmode(_fileno(inputFilePtr), _O_BINARY);
 #endif
+    /* Close stream to filePath */
     fclose(inputFilePtr);
 
     return fileContents;
 }
 
-void write_file(const char *fileName, const char *fileContents) {
+void write_file(const char *filePath, const char *fileContents) {
     FILE *outputFilePtr;
-    size_t fileSize = strlen(fileContents);
 
-    assert_not_equal(outputFilePtr = fopen(fileName, "w+"), NULL);
+    /* Open stream to filePath in write mode */
+    assert_not_equal(outputFilePtr = fopen(filePath, "w+"), NULL);
 
+    /* fileContents is written into the file */
     fputs(fileContents, outputFilePtr);
 
 #ifdef _WIN32
