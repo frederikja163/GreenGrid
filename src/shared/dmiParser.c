245afe89bb10c5ecd07ca2d5d571a39c71c36a5b
Anders Aavild <anders@aavild.dk>
2021-12-07 09:37:51 +0100

    fixed branch maybe v2

---
+++ src/shared/dmiParser.c
@@ -0,0 +1,45 @@
+#include "DMIParser.h"
+
+/*input ninjo2dnudk.json and you will get out an array of windValues and a timestamp for last update*/
+windValue* ParseStringToWind(const char *input, char *lastUpdate)
+{
+    cJSON *json = cJSON_Parse(input);
+    if (json == NULL)
+    {
+        const char *error = cJSON_GetErrorPtr();
+        if (error != NULL)
+        {
+            printf("Error at: %s", error);
+        }
+        return 0;
+    }
+    cJSON *lastUpdateJson = cJSON_GetObjectItem(json, "lastupdate");
+    if (cJSON_IsString(lastUpdateJson) && (lastUpdateJson->valuestring != NULL))
+    {
+        lastUpdate = lastUpdateJson->valuestring;
+        /*printf("Input file last updated: %s\n", lastUpdate);*/
+    }
+    else
+        printf("Failed to get timestamp for last updated\n");
+    windValue *values = (windValue*) calloc(97, sizeof(windValue)); /*timeserie of size 97*/
+    if(values == NULL)
+        return 0;  /* out of memory! */
+    int iterator = 0;
+    cJSON *timeserie = cJSON_GetObjectItem(json, "timeserie");
+    cJSON *hour;
+    cJSON_ArrayForEach(hour, timeserie)
+    {
+        cJSON *time = cJSON_GetObjectItem(hour, "time");
+        cJSON *windSpeed = cJSON_GetObjectItem(hour, "windSpeed");
+        if (!cJSON_IsString(time) || !cJSON_IsNumber(windSpeed))
+        {
+            continue;
+        }
+        values[iterator].windspeed = windSpeed->valuedouble;
+        values[iterator].timestamp = (char*) calloc(20, sizeof(char));
+        strcpy(values[iterator].timestamp, time->valuestring);
+        iterator++;
+    }
+    cJSON_Delete(json);
+    return values;
+}
