b78c31e1d7d829d818f58a1c9cd13e7e3c110d0b
Anders Aavild <anders@aavild.dk>
2021-12-07 09:36:31 +0100

    fixed branch maybe v2

---
+++ src/shared/dmiParser.h
@@ -0,0 +1,17 @@
+#ifndef CO2_DMIPARSER_H
+
+#define CO2_DMIPARSER_H
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <cJSON.h>
+
+typedef struct windValue {
+    char* timestamp;
+    double windspeed;
+} windValue;
+
+windValue* ParseStringToWind(const char *input, char *lastUpdate);
+
+#endif
+
