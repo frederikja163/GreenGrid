843c20e7635617b263222d42c845fcb981aa80de
Anders Aavild <anders@aavild.dk>
2021-12-07 08:27:53 +0100

    finished tests

--- src/tests/AllTests.c
+++ src/tests/AllTests.c
@@ -3,14 +3,14 @@
 #include "CuTest.h"
 
 CuSuite *get_file_io_suite(void);
-CuSuite *DMIParser_suite(void);
+CuSuite *dmi_parser_suite(void);
 
 int run_all_tests(void) {
     CuString *output = CuStringNew();
     CuSuite *suite = CuSuiteNew();
 
     CuSuiteAddSuite(suite, get_file_io_suite());
-    CuSuiteAddSuite(suite, DMIParser_suite());
+    CuSuiteAddSuite(suite, dmi_parser_suite());
 
     CuSuiteRun(suite);
     CuSuiteSummary(suite, output);
