1225399508bf4ab0882639c81feb610d40816a4d
AleksanderRDL <55142567+AleksanderRDL@users.noreply.github.com>
2021-12-08 12:09:16 +0100

    Fixes

--- src/shared/calculateOptimalTime.c
+++ src/shared/calculateOptimalTime.c
@@ -31,19 +31,23 @@ char* find_optimal_time(int activeHours) {
 char* find_lowest_co2(int activeHours, windValue *values, int data_size) {
     int i;
     double currentCO2 = 0;
-    double lowestCO2 = 99999; 
+    double lowestCO2; 
     char *optimalTime = (char*) malloc(20*sizeof(char));
     assert_not_equal(optimalTime = malloc(20), NULL);
     
     for (i = 0; i < data_size; i++) {
         currentCO2 += calculate_co2(values[i].windspeed);
-        if (i >= activeHours-1) {
+        if (i >= activeHours) {
             currentCO2 -= calculate_co2(values[i-activeHours].windspeed);
 
             if (currentCO2 < lowestCO2) {
                 lowestCO2 = currentCO2;
                 strcpy(optimalTime, values[i-activeHours+1].timestamp);
             }
+        }
+        else if (i == activeHours - 1) {
+            lowestCO2 = currentCO2;
+            strcpy(optimalTime, values[0].timestamp);
         }
     }
     return optimalTime;
